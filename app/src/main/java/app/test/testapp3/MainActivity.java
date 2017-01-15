0659469dae75a0872d7d98e7a9c3a87ad0375e01
motherfather <mother@father>
2017-01-16 06:09:02 +0900

    뭔가 안됨... 일단 저장용22222

--- app/src/main/java/app/test/testapp3/MainActivity.java
+++ app/src/main/java/app/test/testapp3/MainActivity.java
@@ -12,6 +12,9 @@ import com.github.kevinsawicki.http.HttpRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.io.Reader;
 import java.util.List;
 import java.util.Map;
@@ -44,14 +47,13 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class JSONTest2 extends JSONTest<Map<String, List<ShopVo>>> { // 서버에서 읽어온 data의 타입을 JSONTest<?> ?에 적는다
+    private class JSONTest2 extends JSONTest<Map<String, List>> { // 서버에서 읽어온 data의 타입을 JSONTest<?> ?에 적는다
     }
 
     private class FetchAsync extends SafeAsyncTask<List<ShopVo>> { // SafeAsyncTask<?> ?에는 onSuccuss(...) 파라미터로 받아올 타입을 적는다
 
         @Override
         public List<ShopVo> call() throws Exception {
-
             Gson gson = new GsonBuilder().create(); // gson 선언
 //            자신의 IP적기
 //            HttpRequest httpRequest = HttpRequest.get("http://192.168.1.15:8088/modeal/shop/test", true, "page", 3); // get 방식
@@ -60,16 +62,7 @@ public class MainActivity extends AppCompatActivity {
             httpRequest.accept(HttpRequest.CONTENT_TYPE_JSON); // 받아 오는 데이터 타입 설정
             httpRequest.contentType(HttpRequest.CONTENT_TYPE_JSON); // 넘겨 주는 데이터 타입 설정
 
-//            Page page = new Page();
-//            page.setPage(Integer.parseInt(editText.getText().toString()));
-//            page.setKeyword(editText2.getText().toString());
-//            page.setFilterCheck(Integer.parseInt(editText3.getText().toString()));
-//
-//            String json2 = gson.toJson(page);
-//            Log.d("!@!@", json2);
-//
 //            httpRequest.form(JSONTest3.class, json2); // post 방식
-
 //            httpRequest.send("page=" + page.getPage());
 
 //            Reader와 비슷하지만 살짝 다름
@@ -78,9 +71,13 @@ public class MainActivity extends AppCompatActivity {
 //            String body = httpRequest.body();
 
             Reader reader = httpRequest.bufferedReader(); // url에서 데이터 가져오기
-            JSONTest2 str = gson.fromJson(reader, JSONTest2.class); // 받아온 json 데이터 객체로 변경
-            Log.d("-----", str.toString());
-            return str.getData().get("list");
+            JSONTest2 jsonTest2 = gson.fromJson(reader, JSONTest2.class); // 받아온 json 데이터 객체로 변경
+            Log.w("=====123", jsonTest2.getData().toString());
+            JSONObject jsonArray = jsonTest2.getA;
+            Log.d("-----", jsonTest2.getData().get("list").getClass() + "");
+            List list = (List)jsonTest2.getData();
+//            return jsonTest.getData().get("list");
+            return null;
         }
 
         @Override
@@ -90,51 +87,17 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onSuccess(List<ShopVo> shopVos) throws Exception {
-            Log.e("-------", "유후");
+            Log.e("-------", shopVos.get(0).getClass() + "");
             String list = "";
-            for (ShopVo shopVo : shopVos) {
-                list += shopVo.getNo() + '\n';
+            for (int i = 0; i < shopVos.size(); i++) {
+                list += shopVos.get(i) + "" + '\n';
             }
-            Log.i("------", list);
+//            for (ShopVo shopVo : shopVos) {
+//                list += shopVo + "" + '\n';
+//            }
+            Log.w("------", list);
         }
     }
 
-    public class Page { // alt + insert
-        private int page;
-        private String keyword;
-        private int filterCheck;
 
-        public int getPage() {
-            return page;
-        }
-
-        public void setPage(int page) {
-            this.page = page;
-        }
-
-        public String getKeyword() {
-            return keyword;
-        }
-
-        public void setKeyword(String keyword) {
-            this.keyword = keyword;
-        }
-
-        public int getFilterCheck() {
-            return filterCheck;
-        }
-
-        public void setFilterCheck(int filterCheck) {
-            this.filterCheck = filterCheck;
-        }
-
-        @Override
-        public String toString() {
-            return "Page{" +
-                    "page=" + page +
-                    ", keyword='" + keyword + '\'' +
-                    ", filterCheck=" + filterCheck +
-                    '}';
-        }
-    }
 }
